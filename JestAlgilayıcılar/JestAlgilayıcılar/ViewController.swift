27613f1decb8d082a3b89af5a06773003958c5b0
Eren Akkoç <ern.akkc@gmail.com>
2026-05-06 09:44:01 +0300

    Jest Algilayiciları

---
+++ JestAlgilayıcılar/JestAlgilayıcılar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JestAlgilayıcılar
+//
+//  Created by Eren Akkoç on 6.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
